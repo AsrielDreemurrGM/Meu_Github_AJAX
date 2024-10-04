c76df0f752e4a0223e02093d1caf5f689b89bd19
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-10-03 23:38:21 -0300

    Criado Código de Coleta de Informações
    - Utilizado o Fetch API;

---
+++ src/scripts/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const avatar = document.getElementById('avatar');
+    const name = document.getElementById('name');
+    const username = document.getElementById('username');
+    const repositories = document.getElementById('repositories');
+    const followers = document.getElementById('followers');
+    const following = document.getElementById('following');
+    const link = document.getElementById('link');
+
+    fetch('https://api.github.com/users/AsrielDreemurrGM')
+        .then(function(answer) {
+            return answer.json();
+        })
+        .then(function(json) {
+            avatar.src = json.avatar_url;
+            name.innerText = json.name;
+            username.innerText = json.login;
+            repositories.innerText = json.public_repos;
+            followers.innerText = json.followers;
+            following.innerText = json.following;
+            link.href = json.html_url;
+        })
+})
